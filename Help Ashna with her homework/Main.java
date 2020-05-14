8b52d43a70a3cd67cfe3713cf6262222d26283bb
Abhiram_47 <43680464+Abhiram6016@users.noreply.github.com>
2020-05-14 22:24:44 +0530

    Test code pushed to repository.

---
+++ Help Ashna with her homework/Main.java
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int b,c;
+  cin>>b>>c;
+  cout<<"a+b="<<b+c<<"\n";
+   cout<<"a-b="<<b-c<<"\n";
+  cout<<"a*b="<<b*c<<"\n";
+  cout<<"a/b="<<b/c<<"\n";
+  cout<<"a%b="<<b%c<<"\n";
+
+}
---
+++ Naming  Ceremony/Main.java
@@ -0,0 +1,7 @@
+#include<iostream>
+int main()
+{
+  char names[6];
+  std::cin>>names;
+  std::cout<<names;
+}
